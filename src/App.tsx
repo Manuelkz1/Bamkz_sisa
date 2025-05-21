d217c46eab5d34408d7c3891bed3b1e71ba7c558
Manuelkz1 <actuvistamoss@gmail.com>
2025-05-21 00:02:52 -0500

    Corregido error de pantalla en blanco en inicio de sesión: agregada ruta /auth

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './components/Home'
 import ProductDetail from './components/ProductDetail'
 import Cart from './components/Cart'
+import { Auth } from './components/Auth'
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/product/:id" element={<ProductDetail />} />
           <Route path="/cart" element={<Cart />} />
+          <Route path="/auth" element={<Auth />} />
         </Routes>
       </div>
     </Router>
